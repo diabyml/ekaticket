cb301f13a3f90aa27819843dae08455962d77f49
diabyml <diaby223.ml@gmail.com>
2023-11-10 20:36:58 +0000

    small change

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -30,7 +30,7 @@ export default function UserButton() {
             as="button"
             className="transition-transform"
             showFallback
-            src=""
+            src={user?.imageUrl}
             size="sm"
           />
         </DropdownTrigger>
